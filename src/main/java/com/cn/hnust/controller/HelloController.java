66e6c6139a1db97ac045469ba483efbbd7ec9408
xiaoyiyibaibai <2367808698@qq.com>
2016-10-10 14:50:56 +0800

    最简单的非注解的springmvc的示例代码。

--- src/main/java/com/cn/hnust/controller/HelloController.java
+++ src/main/java/com/cn/hnust/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.cn.hnust.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jackson.JsonProcessingException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -14,8 +15,13 @@ public class HelloController implements Controller {
 		//通过request对象将信息在页面上展示
 		//req.setAttribute("message", message);
 		ModelAndView view =  new ModelAndView();
-		//
-		return null;
+		//相当于request.setAttribute()，将数据传到页面展示
+		//model数据
+		view.addObject("message", message);
+		//设置视图
+		view.setViewName("hello");
+		//已经在springmvc.xml中添加了前后缀了，即/WEB-INF/jsp/hello.jsp
+		return view;
 	}
 
 }
