01513bd8afbc4a10f1a54b8d905b40cdbb251dc6
xiaoyiyibaibai <2367808698@qq.com>
2016-10-11 11:49:21 +0800

    修改的内容

--- src/main/java/com/cn/hnust/controller/HelloController.java
+++ src/main/java/com/cn/hnust/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.cn.hnust.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.JsonProcessingException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
