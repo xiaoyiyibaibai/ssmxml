403a0f6507bed78b0467f5eca0b25ac3cd91dc23
xiaoyiyibaibai <2367808698@qq.com>
2016-10-12 11:00:39 +0800

    用于验证spring security 的使用。

---
+++ src/main/java/com/cn/hnust/controller/LoginLogoutController.java
@@ -0,0 +1,54 @@
+package com.cn.hnust.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.ModelMap;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMethod;  
+import org.springframework.web.bind.annotation.RequestParam;  
+  /**
+   * 用于测试spring security使用。
+   * @author xiaodonghong
+   *
+   * 2016年10月12日 上午10:53:50
+   */
+@Controller  
+@RequestMapping("auth")  
+public class LoginLogoutController {  
+    protected static Logger logger = LoggerFactory.getLogger(LoginLogoutController.class);  
+  
+    /** 
+     * 指向登录页面 
+     */  
+    @RequestMapping(value = "/login", method = RequestMethod.GET)  
+    public String getLoginPage(  
+            @RequestParam(value = "error", required = false) boolean error,  
+            ModelMap model) {  
+  
+        logger.debug("Received request to show login page");  
+  
+        if (error == true) {  
+            // Assign an error message  
+            model.put("error",  
+                    "You have entered an invalid username or password!");  
+        } else {  
+            model.put("error", "");  
+        }  
+        return "loginpage";  
+  
+    }  
+  
+    /** 
+     * 指定无访问额权限页面 
+     *  
+     * @return 
+     */  
+    @RequestMapping(value = "/denied", method = RequestMethod.GET)  
+    public String getDeniedPage() {  
+  
+        logger.debug("Received request to show denied page");  
+  
+        return "deniedpage";  
+  
+    }  
+}  
