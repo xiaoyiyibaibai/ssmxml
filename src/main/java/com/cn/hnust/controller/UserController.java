8b9ed8b2dd3c4698cc3c03520266359322fd7363
xiaoyiyibaibai <2367808698@qq.com>
2016-10-12 14:17:23 +0800

    url的访问地址。

--- src/main/java/com/cn/hnust/controller/UserController.java
+++ src/main/java/com/cn/hnust/controller/UserController.java
@@ -15,9 +15,16 @@ import com.cn.hnust.service.IUserService;
 public class UserController {
 	@Resource
 	private IUserService userService;
-	
+	/**
+	 * url的访问地址：http://localhost:8080/springmvc//user/showUser?id=9
+	 * @param request
+	 * @param model
+	 * @return
+	 * 2016年10月12日 下午2:16:49
+	 */
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model){
+		
 		int userId = Integer.parseInt(request.getParameter("id"));
 		User user = this.userService.getUserById(userId);
 		model.addAttribute("user", user);
